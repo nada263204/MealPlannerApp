b4d5b9e4d856442c56b7a3c26272660751ab9d24
nada gharib <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>
2025-02-12 07:23:41 +0200

    fixing dashboard with fragmets and add bottom navigation bar

--- app/src/main/java/com/example/mealplannerapp/DashboardActivity.java
+++ app/src/main/java/com/example/mealplannerapp/DashboardActivity.java
@@ -1,103 +1,54 @@
 package com.example.mealplannerapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
-import com.bumptech.glide.Glide;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import java.util.List;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
-
-    public static final String url = "https://www.themealdb.com/api/json/v1/1/";
-    private static final String TAG = "DashboardActivity";
-
-    private ImageView mealImage;
-    private TextView mealTitle, mealId;
-    private CardView mealCard;
-    private String mealIdValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
         String username = getIntent().getStringExtra("USERNAME");
-        TextView usernameTextView = findViewById(R.id.usernameTextView);
-        if (username != null) {
-            usernameTextView.setText("Hi, " + username);
-        }
 
-        mealImage = findViewById(R.id.mealImage);
-        mealTitle = findViewById(R.id.mealTitle);
-        mealId = findViewById(R.id.mealId);
-        mealCard = findViewById(R.id.mealCard);
+        HomeFragment homeFragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("USERNAME", username);
+        homeFragment.setArguments(bundle);
 
-        fetchMeal();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, homeFragment)
+                .commit();
 
-        mealCard.setOnClickListener(view -> {
-            if (mealIdValue != null) {
-                Intent intent = new Intent(DashboardActivity.this, MealDetailActivity.class);
-                intent.putExtra("MEAL_ID", mealIdValue);
-                startActivity(intent);
-            } else {
-                Toast.makeText(DashboardActivity.this, "Meal ID is not available", Toast.LENGTH_SHORT).show();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+                selectedFragment.setArguments(bundle);
+            } else if (itemId == R.id.nav_search) {
+                selectedFragment = new SearchFragment();
+            } else if (itemId == R.id.nav_calendar) {
+                selectedFragment = new CalenderFragment();
+            } else if (itemId == R.id.nav_favorites) {
+                selectedFragment = new FavoritesFragment();
             }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
         });
     }
 
-    private void fetchMeal() {
-        Retrofit retrofitInstance = new Retrofit.Builder()
-                .baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        MealService mealService = retrofitInstance.create(MealService.class);
-        Call<MealResponse> call = mealService.getMeal();
-
-        call.enqueue(new Callback<MealResponse>() {
-            @Override
-            public void onResponse(Call<MealResponse> call, Response<MealResponse> response) {
-                if (response.isSuccessful() && response.body() != null && response.body().getMeals() != null) {
-                    List<Meal> meals = response.body().getMeals();
-                    if (!meals.isEmpty()) {
-                        Meal meal = meals.get(0);
-
-                        mealIdValue = meal.getIdMeal();
-
-                        mealTitle.setText(meal.getStrMeal());
-                        mealId.setText("Meal ID: " + mealIdValue);
-
-                        Glide.with(DashboardActivity.this)
-                                .load(meal.getStrMealThumb())
-                                .into(mealImage);
-                    } else {
-                        Toast.makeText(DashboardActivity.this, "No meal found", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(DashboardActivity.this, "No meal found", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<MealResponse> call, Throwable throwable) {
-                Log.e(TAG, "API Error", throwable);
-                Toast.makeText(DashboardActivity.this, "Failed to fetch data", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 }
---
+++ app/src/main/java/com/example/mealplannerapp/HomeFragment.java
@@ -0,0 +1,115 @@
+package com.example.mealplannerapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class HomeFragment extends Fragment {
+    public static final String url = "https://www.themealdb.com/api/json/v1/1/";
+    private static final String TAG = "HomeFragment";
+
+    public HomeFragment(){}
+
+    private ImageView mealImage;
+    private TextView mealTitle, mealId,usernameTextView;
+    private CardView mealCard;
+    private String mealIdValue;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+            usernameTextView = view.findViewById(R.id.usernameTextView);
+
+            Bundle args = getArguments();
+            if (args != null) {
+                String username = args.getString("USERNAME", "User");
+                usernameTextView.setText("Hi, " + username + "!");
+            }
+
+
+        mealImage = view.findViewById(R.id.mealImage);
+        mealTitle = view.findViewById(R.id.mealTitle);
+        mealId = view.findViewById(R.id.mealId);
+        mealCard = view.findViewById(R.id.mealCard);
+
+        fetchMeal();
+
+        mealCard.setOnClickListener(v -> {
+            if (mealIdValue != null) {
+                Intent intent = new Intent(getActivity(), MealDetailActivity.class);
+                intent.putExtra("MEAL_ID", mealIdValue);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getActivity(), "Meal ID is not available", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+    private void fetchMeal() {
+        Retrofit retrofitInstance = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MealService mealService = retrofitInstance.create(MealService.class);
+        Call<MealResponse> call = mealService.getMeal();
+
+        call.enqueue(new Callback<MealResponse>() {
+            @Override
+            public void onResponse(Call<MealResponse> call, Response<MealResponse> response) {
+                if (response.isSuccessful() && response.body() != null && response.body().getMeals() != null) {
+                    List<Meal> meals = response.body().getMeals();
+                    if (!meals.isEmpty()) {
+                        Meal meal = meals.get(0);
+
+                        mealIdValue = meal.getIdMeal();
+
+                        mealTitle.setText(meal.getStrMeal());
+                        mealId.setText("Meal ID: " + mealIdValue);
+
+                        Glide.with(requireContext())
+                                .load(meal.getStrMealThumb())
+                                .into(mealImage);
+                    } else {
+                        Toast.makeText(getActivity(), "No meal found", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "No meal found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MealResponse> call, Throwable throwable) {
+                Log.e(TAG, "API Error", throwable);
+                Toast.makeText(getActivity(), "Failed to fetch data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
